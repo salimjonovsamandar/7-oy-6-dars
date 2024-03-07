d9895c3f87447fc3037c9909e20225fc46e41fd5
salimjonovsamandar <149486604+salimjonovsamandar@users.noreply.github.com>
2024-03-08 04:50:40 +0500

    Tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,119 @@
-import './App.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [pomodoro, setPomodoro] = useState(0)
-  const [short, setShort] = useState(0)
-  const [long, setLong] = useState(0)
-  const [isActive, setIsActive] = useState("")
-  const [timerValue, setTimerValue] = useState("00:00")
+  const [isActive, setIsActive] = useState(false);
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    let timer = {}
-    if (localStorage.getItem("timer")) {
-      timer = JSON.parse(localStorage.getItem("timer"))
+    if (localStorage.getItem("time")) {
+      setMinutes(JSON.parse(localStorage.getItem("time")));
     } else {
-      timer = {
-        pomodoro: "10:00",
-        short: "05:00",
-        long: "15:00"
-      }
+      localStorage.setItem("time", JSON.stringify(minutes));
+    }
+  }, []);
 
-      localStorage.setItem("timer", JSON.stringify(timer))
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive) {
+      intervalId = setInterval(() => {
+        if (seconds === 0 && minutes === 0) {
+          clearInterval(intervalId);
+          setIsActive(false);
+          console.log("Timer tugadi");
+        } else if (seconds === 0) {
+          setMinutes((prevMinutes) => prevMinutes - 1);
+          setSeconds(59);
+        } else {
+          setSeconds((prevSeconds) => prevSeconds - 1);
+        }
+      }, 1000);
     }
 
-    setPomodoro(timer.pomodoro)
-    setShort(timer.short)
-    setLong(timer.long)
-    setIsActive("pomodoro")
+    return () => clearInterval(intervalId);
+  }, [isActive, minutes, seconds]);
 
-    setTimerValue(timer.pomodoro)
+  const handleStartStop = () => {
+    setIsActive((prevIsActive) => !prevIsActive);
+  };
 
-  }, [])
-  function handleClick(arg) {
-    if (arg == "short") {
-      setTimerValue(short)
+  const handleReset = () => {
+    setMinutes(25);
+    setSeconds(0);
+    setIsActive(false);
+  };
+
+  function heandleType(type) {
+    handleReset();
+    if ("pomodoro" == type) {
+      setMinutes(25);
     }
-    if (arg == "long") {
-      setTimerValue(long)
+    if ("short" == type) {
+      setMinutes(5);
     }
-    setIsActive(arg)
+    if ("long" == type) {
+      setMinutes(15);
+    }
   }
 
-
   return (
-    <div className='container bg-slate-200 p-5 h-dvh mx-auto'>
-      <div className=' w-1/3 rounded-xl p-5 mx-auto bg-slate-500'>
-        <h1 className='text-center pb-5 text-white font-bold text-4xl '>POMODORO</h1>
-        <div className='text-center flex justify-between w-2/3 cursor-pointer pb-5 mx-auto'>
-          <span onClick={() => { handleClick("pomodoro") }} className={`text-slate-900 font-bold text-lg p-2 ${isActive == "pomodoro" ? "bg-slate-600 rounded-xl  first-line:text-white" : ""}`}>Work</span>
-          <span onClick={() => { handleClick("short") }} className={`text-slate-900 font-bold text-lg p-2 ${isActive == "short" ? "bg-slate-600 rounded-xl  first-line:text-white" : ""}`}>Short Break</span>
-          <span onClick={() => { handleClick("long") }} className={`text-slate-900 font-bold text-lg p-2 ${isActive == "long" ? "bg-slate-600 rounded-xl  first-line:text-white" : ""}`}>Long Break</span>
-        </div>
-        <div>
-          <div className='w-72 h-72 bg-slate-200 rounded-full mx-auto'>
-            <h1 className='text-center text-8xl pt-16 text-slate-900 font-bold'>{timerValue}</h1>
-            <p className=' text-center text-4xl mt-8 text-slate-900 cursor-pointer'>Start</p>
+    <div className="container">
+      <div className="pomodoro">
+        <header>
+          <div className="logo">
+            <h2>Pomofocus</h2>
           </div>
-        </div>
-        <div className='text-slate-900'>
-          <i className="fa-solid fa-gear cursor-pointer mt-5 text-4xl block text-center"></i>
-        </div>
+          <div className="btns">
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path
+                  fill="white"
+                  d="M32 32c17.7 0 32 14.3 32 32V400c0 8.8 7.2 16 16 16H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H80c-44.2 0-80-35.8-80-80V64C0 46.3 14.3 32 32 32zM160 224c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm128-64V320c0 17.7-14.3 32-32 32s-32-14.3-32-32V160c0-17.7 14.3-32 32-32s32 14.3 32 32zm64 32c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V224c0-17.7 14.3-32 32-32zM480 96V320c0 17.7-14.3 32-32 32s-32-14.3-32-32V96c0-17.7 14.3-32 32-32s32 14.3 32 32z"
+                />
+              </svg>
+              <span>Report</span>
+            </button>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path
+                  fill="white"
+                  d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
+                />
+              </svg>
+              <span>Settings</span>
+            </button>
+          </div>
+        </header>
+        <main>
+          <div className="time">
+            <div className="btns">
+              <button onClick={() => heandleType("pomodoro")}>Pomodoro</button>
+              <button onClick={() => heandleType("short")}>Short Breake</button>
+              <button onClick={() => heandleType("long")}>Long Breake</button>
+            </div>
+            <h2 className="timer">
+              {minutes < 10 ? `0${minutes}` : minutes}:
+              {seconds < 10 ? `0${seconds}` : seconds}
+            </h2>
+            <div className="btns">
+              <button className="start" onClick={handleStartStop}>
+                {isActive ? "Stop" : "Start"}
+              </button>
+              {isActive ? (
+                <button className="start" onClick={handleReset}>
+                  Reset
+                </button>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </main>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
